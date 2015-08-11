1d340a5807016251582448de507c63888b6c8f30
Kenji Endo <colvin.endo@gmail.com>
2015-08-10 22:32:45 -0400

    Modal windows

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+/*$(window).load(function(){
+    $('.img-responsive').on('click',function()
+        {
+            var sr=$(this).attr('src'); 
+            $('#mimg').attr('src',sr);
+            $('#myModal').modal('show');
+        });
+});
+*/
